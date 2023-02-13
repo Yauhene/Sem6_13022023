6e315f176047188fbb761b57a5a7b21211e10a46
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-13 21:26:25 +0300

    add Ex03

---
+++ Ex02_/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+int EnterNumber(string name)
+ {
+    int num;
+
+    Console.Write($"Введите число {name}: ");
+    while(!int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine("Ерунда какая-то... Ощущение, что введено не целое число.");
+        Console.Write($"Ну-ка, еще разок! {name} = ");
+    }
+    return  num;
+}
+
+void Execution(int a, int b, int c)
+{
+	if ((a < b + c) && (b < a + c) && (c < a + b))
+	{
+		System.Console.WriteLine("Даннный набор размеров сторон адекватный");
+	}
+	else
+	{
+		System.Console.WriteLine("Даннный набор размеров сторон неадекватный");
+	}
+}
+
+int a = EnterNumber("a");
+int b = EnterNumber("b");
+int c = EnterNumber("c");
+
+System.Console.WriteLine(a);
+System.Console.WriteLine(b);
+System.Console.WriteLine(c);
+
+Execution(a, b, c);
+Console.WriteLine("Hello, World!");
---
+++ Ex03_/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное
+// 45 --> 101101
+// 3 --> 11
+// 2 --> 10
+Console.WriteLine("Hello, World!");
+
+int EnterNumber(string name)
+ {
+    int num;
+
+    Console.Write($"Введите число {name}: ");
+    while(!int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine("Ерунда какая-то... Ощущение, что введено не целое число.");
+        Console.Write($"Ну-ка, еще разок! {name} = ");
+    }
+    return  num;
+}
+
+string Conversion(int dec)
+{
+	string bin = "";
+	while (dec > 0)
+	{
+		bin = dec % 2 + bin;
+		dec = dec / 2;
+		// if (dec % 2 == 0)
+		// {
+		// 	bin = "0" + bin;
+		// }
+		// else
+		// {
+		// 	bin = "1" + bin;
+		// }
+	}
+	System.Console.WriteLine(bin);
+	return bin;
+}
+
+Conversion(EnterNumber(""));
