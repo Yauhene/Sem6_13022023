5b83ece63314de23eef8e96b07c6544e02709a03
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-13 20:36:22 +0300

    add code

--- Ex01_/Program.cs
+++ Ex01_/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int [] arr = new int [5];
+
+RandomArr(arr);
+Console.WriteLine(string.Join(" ",arr));
+SwithArr(arr);
+Console.WriteLine(string.Join(" ",arr));
+void RandomArr(int [] arr)
+{
+    Random random = new Random();
+
+    for(int i=0; i<arr.Length;i++)
+    {
+        arr[i]=random.Next();
+    }
+
+}
+
+void SwithArr(int [] arr)
+{
+    int a=0;
+    for(int i=0;i<arr.Length/2;i++)
+    {
+        a = arr[i];
+        arr[i] = arr[arr.Length-1-i];
+        arr[arr.Length-1-i] = a;
+    }
+}
