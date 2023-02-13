8684fe9cac9ec48d3172ee405c9b9b0ef306834a
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-13 22:22:29 +0300

    add Ex05

---
+++ Ex04/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+  кому  Все
+// Функция принимает число из консоли и обходит ошибку ввода
+int IntInput(string intName) // функция принимает число из консоли, преобразуя его в целое
+                            // зело ругаеццо при косячном вводе
+{
+    int num;
+
+    Console.Write($"Введите число {intName}: ");
+    while(!int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine("Ерунда какая-то... Ощущение, что введено не целое число.");
+        Console.Write($"Ну-ка, еще разок! {intName} = ");
+    }
+    return  num;
+}
+Игорь Серебряков 22:05
+int EnterNumber(string name)
+{
+	int num;
+
+	Console.Write($"Введите число {name}: ");
+	while (!int.TryParse(Console.ReadLine(), out num))
+	{
+		Console.WriteLine("Ерунда какая-то... Ощущение, что введено не целое число.");
+		Console.Write($"Ну-ка, еще разок! {name} = ");
+	}
+	return num;
+}
+
+void Fibonacci(int num)
+{
+	int[] arr = new int [num];
+	
+	if (num > 2)
+	{
+		arr[0] = 0;
+		arr[1] = 1;
+		for (int i = 2; i < num; i++)
+		{
+		arr[i] = arr[i - 2] + arr[i - 1];
+		}
+		System.Console.WriteLine(string.Join(" ", arr));
+	}
+	else
+	{
+   	System.Console.WriteLine("Введите число больше 2");
+	}		
+}
+
+Fibonacci(EnterNumber(""));
---
+++ Ex05/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] CreateRandomArray(int lenght, int min, int max)
+{
+	int[] array = new int[lenght];
+	Random random = new Random();
+	for (int i = 0; i < lenght; i++)
+	{
+		array[i] = random.Next(min, max + 1);
+	}
+	return array;
+}
+
+int[] CopyArray(int[] array)
+{
+	int[] newArr = new int[array.Length];
+
+	for (int i = 0; i < array.Length; i++)
+	{
+	newArr[i] = array[i];	
+	}
+
+	return newArr;
+}
+
+void PrintNumbers(int[] array)
+{
+	System.Console.WriteLine(string.Join(", ", array));
+}
+
+int[] array = CreateRandomArray(10, 0, 100);
+PrintNumbers(array);
+int[] newArr = CopyArray(array);
+PrintNumbers(newArr);
